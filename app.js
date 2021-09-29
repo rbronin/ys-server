92913fc29d92b47ff7b5fd22b57d2befae7296de
Ravi Bharti <ravics1718@gmail.com>
2021-09-29 22:23:34 +0530

    Feat: Add Precommit git hooks with Eslint & Prettier

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
+"use strict";
 require("dotenv").config();
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
-const bodyParser = require("body-parser");
 const compression = require("compression");
 const InitiateMongoDB = require("./config/db.config");
 
@@ -24,7 +24,7 @@ app.use(compression());
 app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/api", testRoutes);
